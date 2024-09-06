466bbd82118ebf07a47af132237df76ea3068e89
Jhon Apaza <jhonangelapazapariapaza@gmail.com>
2024-09-06 04:41:45 -0500

    Primer Intento

---
+++ src/laboratoriosesion3/Calculadora.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratoriosesion3;
+
+/**
+ *
+ * @author user
+ */
+public class Calculadora {
+    private double numero1;
+    private double numero2;
+
+    public void setNumero1(double numero1) {
+        this.numero1 = numero1;
+    }
+
+    public void setNumero2(double numero2) {
+        this.numero2 = numero2;
+    }
+
+    public double sumar() {
+        return numero1 + numero2;
+    }
+
+    public double restar() {
+        return numero1 - numero2;
+    }
+
+    public double multiplicar() {
+        return numero1 * numero2;
+    }
+
+    public double dividir() {        
+        return numero1 / numero2;        
+    }
+}
---
+++ src/laboratoriosesion3/LaboratorioSesion3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratoriosesion3;
+
+/**
+ *
+ * @author user
+ */
+public class LaboratorioSesion3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PantallaCalculadora pantalla = new PantallaCalculadora();
+        pantalla.setVisible(true);
+    }
+    
+}
